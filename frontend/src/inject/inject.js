998cd539ec590b558accd8d1a65c109fba891a58
testlolla <72363318+testlolla@users.noreply.github.com>
2022-01-22 15:05:44 -0500

    Update inject.js

--- frontend/src/inject/inject.js
+++ frontend/src/inject/inject.js
@@ -9,22 +9,75 @@ function inject(injection_script) {
 const popup_url = chrome.runtime.getURL("popup.html");
 console.log('hello world');
 function addPopup() {
+    
     var iframe  = document.createElement('iframe');
     iframe.src  = popup_url;
     iframe.className = 'hack-popup';
-    document.body.appendChild (iframe);
+
     document.body.innerHTML += `<style>
     .hack-popup { 
         position: absolute;
         top: 8px;
-        right: 16px;
+        left: 16px;
+        width: 100px;
+        height: 100px;
         border-width: 0px;
         border-style: none;
         z-index: 1000;
-        border-radius: 0.5rem;
-    }</style>`;
+        border-radius: 10rem;
+    }</style>
+    `;
+    document.body.appendChild(iframe);
+
+    var mousePosition;
+    var offset = [0,0];
+    var div;
+    var isDown = false;
+
+    div = document.createElement("div");
+    div.style.position = "absolute";
+    div.style.left = "16px";
+    div.style.top = "8px";
+    div.style.width = "10px";
+    div.style.height = "10px";
+    div.style.background = "red";
+    div.style.color = " ";
+    div.style.zIndex = 1000;
+    div.style.borderRadius = "10rem";
+    
+    document.body.appendChild(div);
+
+    div.addEventListener('mousedown', function(e) {
+        isDown = true;
+        offset = [
+            div.offsetLeft - e.clientX,
+            div.offsetTop - e.clientY
+        ];
+    }, true);
+
+    document.addEventListener('mouseup', function() {
+        isDown = false;
+    }, true);
+
+    document.addEventListener('mousemove', function(event) {
+        event.preventDefault();
+        if (isDown) {
+            mousePosition = {
+    
+                x : event.clientX,
+                y : event.clientY
+    
+            };
+            div.style.left = (mousePosition.x + offset[0]) + 'px';
+            div.style.top  = (mousePosition.y + offset[1]) + 'px';
+            iframe.style.left = (mousePosition.x + offset[0]) + 'px';
+            iframe.style.top  = (mousePosition.y + offset[1]) + 'px';
+        }
+    }, true);   
 
 }
+
+
 addPopup();
 console.log(document);
 // inject(function () {
